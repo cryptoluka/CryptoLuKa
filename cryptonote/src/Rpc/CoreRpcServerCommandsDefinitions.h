b29ac4bf3408a148975ace3805460bb36b778db7
theboldtoken <35311150+theboldtoken@users.noreply.github.com>
2018-07-04 23:49:30 -0600

    Updated RPC - add block difficulty for explorer

--- cryptonote/src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ cryptonote/src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -478,6 +478,7 @@ struct f_block_short_response {
 	std::string hash;
 	uint64_t tx_count;
 	uint64_t cumul_size;
+	uint64_t difficulty;
 
 	void serialize(ISerializer &s) {
 		KV_MEMBER(timestamp)
@@ -485,6 +486,7 @@ struct f_block_short_response {
 			KV_MEMBER(hash)
 			KV_MEMBER(cumul_size)
 			KV_MEMBER(tx_count)
+			KV_MEMBER(difficulty)
 	}
 };
 
